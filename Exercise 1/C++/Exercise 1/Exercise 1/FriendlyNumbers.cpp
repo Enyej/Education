553b949d1c7b92784191cb67576016daec4f8955
Enyej <sergej_baba@mail.ru>
2021-02-21 11:44:40 +0800

    add FriendlyNumbers

---
+++ Exercise 1/C++/Exercise 1/Exercise 1/FriendlyNumbers.cpp
@@ -0,0 +1,25 @@
+#include "FriendlyNumbers.h"
+#include <iostream>
+#include <list>
+
+void FriendlyNumbers::printNumbers(int start, int end)
+{
+    std::list<Pair> lst;
+    for (int i = start; i < end; i++)
+        lst.push_back(Pair(i, getDevisorsSum(i)));
+           
+    for (Pair p : lst)
+        for (Pair p1 : lst)
+            if (p.num == p1.devisorsSum && p.devisorsSum == p1.num)
+                std::cout << p.toString() << std::endl;
+}
+
+int FriendlyNumbers::getDevisorsSum(int num)
+{
+    int sum = 0;
+    for (int i = 1; i < num / 2 + 1; i++) {
+        if (num % i == 0)
+            sum += i;
+    }
+    return sum;
+}
