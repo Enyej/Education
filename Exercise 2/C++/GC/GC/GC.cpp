7ea569933a6e2a94c63e567f32fb1210ae9d5b43
Enyej <sergej_baba@mail.ru>
2021-03-06 12:48:39 +0800

    GC done

---
+++ Exercise 2/C++/GC/GC/GC.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+
+
+using namespace std;
+
+int main()
+{
+    char myword[] = { 'C', 'c', 'G', 'g' };
+    cout << "Enter the line\n";
+    string input;
+    cin >> input;
+    float cnt = 0;
+    for (int i = 0; i < input.size(); i++)
+    {
+
+        char* foo = std::find(std::begin(myword), std::end(myword), input[i]);
+        if (foo != std::end(myword)) {
+            cnt++;
+        }
+    }
+
+    cout << cnt / input.size() * 100;
+
+
+
+
+
+}
+
+
